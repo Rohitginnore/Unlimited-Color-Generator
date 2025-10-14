c730419848ee472467fad57cc074624318fe5011
Rohit <ginnorerohit@gmail.com>
2025-10-14 16:04:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
 // generate a Random color;
 
 
-console.log(Math.random());
+
+const changeColor = function(){
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+  for(let i = 0; i<6; i++){
+    color = color+hex[Math.floor(Math.random()*16)];
+  }  
+
+  return color;
+
+  } 
+const Start = document.querySelector('#start');
+const stop = document.querySelector("#stop");
+
+let interval
+  const startChangingColor = function(){
+    if(interval == null){
+      interval = setInterval(changeBGColors,1000);
+    }
+    function changeBGColors(){
+        document.body.style.backgroundColor = changeColor();
+      }
+  };
+//   console.log(startChangingColor());
+  
+
+  const stopChangingColor = function(){
+    clearInterval(interval);
+    interval = null;
+  }
+
+  Start.addEventListener('click',startChangingColor);
+  stop.addEventListener('click',stopChangingColor);
